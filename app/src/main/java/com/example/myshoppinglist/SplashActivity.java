f93e4aceafb17be126b2258602579fd12cf06eb2
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2022-08-23 09:09:58 +0100

    Put splash screen as launcher activity

--- app/src/main/java/com/example/myshoppinglist/SplashActivity.java
+++ app/src/main/java/com/example/myshoppinglist/SplashActivity.java
@@ -2,7 +2,9 @@ package com.example.myshoppinglist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // hide the support activity bar
+        getSupportActionBar().hide();
+
+        // this will finally load the main activity
+        final Intent i = new Intent(SplashActivity.this, MainActivity.class);
+        //but after a delay of 100
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(i);
+                finish();
+            }
+        }, 1000);
     }
 }
