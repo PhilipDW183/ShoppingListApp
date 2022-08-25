e80b09ae39fbc476c60e937870fbfb807fbd4836
Philip Wilkinson <philip.davidwilkinson@gmail.com>
2022-08-25 09:38:12 +0100

    Make databasehandler public

--- app/src/main/java/com/example/myshoppinglist/Utils/DatabaseHandler.java
+++ app/src/main/java/com/example/myshoppinglist/Utils/DatabaseHandler.java
@@ -26,7 +26,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     private SQLiteDatabase db;
 
-    private DatabaseHandler(Context context){
+    public DatabaseHandler(Context context){
         super(context, NAME, null, VERSION);
     }
 
